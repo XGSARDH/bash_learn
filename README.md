4e056fa8a4ea1b3a3216d20b95f8121d95a10b25
XGSARDH <xgsardh@163.com>
2025-01-25 02:22:35 +0800

    完善上传功能, 增加历史记录区域

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # picgo自动上传图片脚本
 
+> 参考资料
+> [PicGo-Core-Doc](https://picgo.github.io/PicGo-Core-Doc/)
+> [软连接创建和拷贝](https://blog.csdn.net/forgetjoker/article/details/115067384)
+
 可以按照环境中的picgo自动上传图片
 
 ## 配置
 
 需要在该项目目录下创建 **picgo** 的软连接.
+
+*log* 目录中的 **shell.log** 会保存最近一次调用该脚本的输出
--- picgo_upload_auto_name.sh
+++ picgo_upload_auto_name.sh
@@ -1,42 +1,71 @@
 #!/bin/bash
 
-# 获取当前时间，精确到纳秒
-current_time=$(date +%s%N)
-
-# 分离秒和纳秒部分
-seconds=${current_time:0:10}  # 前10位是秒
-nanoseconds=${current_time:10:15}  # 剩余的部分是纳秒
-nanoseconds=${nanoseconds:0:5}
-
-# 将秒部分转换为人类可读的日期时间格式
-human_readable_time=$(date -d @$seconds +"%Y-%m-%d %H:%M:%S")
-
-# 输出最终格式化时间
-result="$human_readable_time $nanoseconds"
-result=$(echo "${result}" | sed 's/[^0-9]//g')
-echo "result = ${result}"
-
-# 自动生成图片名字
-image_name="image-${result}.png"
-echo "image_name = ${image_name}"
-
 # 该shell脚本所在目录获取
 shell_file_path=$(cd $(dirname $0); pwd)
-echo "shell_file_path = ${shell_file_path}"
-
-# 所需要上传图片的路径获取
-image_path="$(pwd)/$1"
-echo "image_path = ${image_path}"
 
 # image图片文件夹
-image_folder_path="${shell_file_path}/image"
-echo "image_folder_path = ${image_folder_path}"
+upload_image_folder_path="${shell_file_path}/image"
+if [ ! -d "${upload_image_folder_path}" ]; then
+    mkdir ${upload_image_folder_path}
+fi
 
 # picgo所在路径, 默认所在目录与该shell脚本所在目录一致
 picgo_path="${shell_file_path}/picgo"
-echo "picgo_path = ${picgo_path}"
+# 所需要上传图片的路径获取, 要判断传入参数
+if [[ "$1" == /* ]]; then
+    origin_image_path="$1"
+else
+    origin_image_path="$(pwd)/$1"
+fi
 
-cp ${image_path} ${image_folder_path}/${image_name}
+# 获取当前时间，精确到纳秒
+current_time=$(date +%s%N)
+# 分离秒和纳秒部分
+# 前10位是秒
+seconds=${current_time:0:10}
+# 剩余的部分是纳秒, 这里后面保留前5位是为了和typora的默认上传格式保持一致
+nanoseconds=${current_time:10:15}
+nanoseconds=${nanoseconds:0:5}
+# 将秒部分转换为人类可读的日期时间格式
+human_readable_time=$(date -d @$seconds +"%Y-%m-%d %H:%M:%S")
+# 输出最终格式化时间, 并且删除除去数字以外的字符
+upload_time="$human_readable_time $nanoseconds"
+upload_time=$(echo "${upload_time}" | sed 's/[^0-9]//g')
+# 生成上传图片名字
+upload_image_name="image-${upload_time}.png"
+
+# 进行图片备份
+cp ${origin_image_path} ${upload_image_folder_path}/${upload_image_name}
 
 # picgo执行上传操作
-${picgo_path} upload ${image_folder_path}/${image_name}
+log_folder_path=${shell_file_path}/log
+log_path=${log_folder_path}/shell.log
+if [ ! -d "${log_folder_path}" ]; then
+    mkdir ${log_folder_path}
+fi
+if [ ! -e "${log_path}" ]; then
+    touch ${log_path}
+fi
+${picgo_path} u ${upload_image_folder_path}/${upload_image_name} >| ${log_path}
+
+# 处理URL
+output=$(cat ${log_path} | grep "http")
+URL=${output}
+
+# 集中输出
+echo ""
+echo "upload_time = ${upload_time}"
+echo "origin_image_path = ${origin_image_path}"
+echo "upload_image_name = ${upload_image_name}"
+echo "picgo_path = ${picgo_path}"
+echo "shell_file_path = ${shell_file_path}"
+echo "upload_image_folder_path = ${upload_image_folder_path}"
+echo "log_path = ${log_path}"
+echo ""
+
+if [ -n "$URL" ]; then
+    echo "URL = "
+    echo "${URL}"
+else
+    echo "ERROR"
+fi
